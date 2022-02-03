dea905b7bc682477dd7183252d19f0608af2ee8c
Hashem Nazar Ono <hashem.nazar@gmail.com>
2022-02-03 09:30:21 +0900

    updated logic on linemsg controller

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -20,55 +20,136 @@ class LineMessengerController extends Controller
 
         // そこからtypeをとりだし、$message_typeに代入
         $message_type = $inputs['events'][0]['type'];
-        Log::info("LOG: the msg type is " . $message_type);
+        Log::info("LOG: message type is " . $message_type);
+
+        // LINEBOTSDKの設定
+        $http_client = new CurlHTTPClient(config('services.line.channel_token'));
+        $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
+
+        // LINEのユーザーIDをuserIdに代入
+        $userId = $request['events'][0]['source']['userId'];
+
+        // userIdがあるユーザーを検索
+        $user = User::where('line_id', $userId)->first();
+
+        // もし見つからない場合は、データベースに保存
+        if ($user == NULL) {
+            $profile = $bot->getProfile($userId)->getJSONDecodedBody();
+
+            $user = new User();
+            $user->provider = 'line';
+            $user->line_id = $userId;
+            $user->name = $profile['displayName'];
+            $user->save();
+        }
+
+        // タイプごとに分岐
+        switch ($message_type) {
+
+                // メッセージ受信
+            case 'message':
+
+                // 返答に必要なトークンを取得
+                $reply_token = $inputs['events'][0]['replyToken'];
+
+                // define message content
+                $message_content = $inputs['events'][0]['message']['text'];
+
+                // The message to send
+                $message_data = "メッセージありがとうございます。ただいま準備中です";
+
+                // if message content is `確認`
+                if ($message_content == '確認') {
+                    $message_data = 'ご確認ありがとうございます';
+                }
+
+                // LINEの投稿処理
+                $response = $bot->replyText($reply_token, $message_data);
+
+                // Succeeded
+                if ($response->isSucceeded()) {
+                    Log::info('LOG: the message is sent');
+                    break;
+                }
+                // Failed
+                Log::error($response->getRawBody());
+                break;
+
+                // 友だち追加 or ブロック解除
+            case 'follow':
+
+                // ユーザー固有のIDを取得
+                $mid = $request['events'][0]['source']['userId'];
+
+                // ユーザー固有のIDはどこかに保存しておいてください。メッセージ送信の際に必要です。
+                LineUser::updateOrCreate(['line_id' => $mid]);
+                Log::info("ユーザーを追加しました。 user_id = " . $mid);
+                break;
+
+                // グループ・トークルーム参加
+            case 'join':
+                Log::info("グループ・トークルームに追加されました。");
+                break;
+
+                // グループ・トークルーム退出
+            case 'leave':
+                Log::info("グループ・トークルームから退出させられました。");
+                break;
+
+                // ブロック
+            case 'unfollow':
+                Log::info("ユーザーにブロックされました。");
+                break;
+
+            default:
+                Log::info("the type is" . $message_type);
+                break;
+        }
+
+        return;
 
 
-        // define message content
-        $message_content = $inputs['events'][0]['message']['text'];
+
 
         // メッセージが送られた場合、$message_typeは'message'となる。その場合処理実行。
-        if ($message_type == 'message') {
+        // if ($message_type == 'message') {
 
-            // replyTokenを取得
-            $reply_token = $inputs['events'][0]['replyToken'];
+        //     // replyTokenを取得
+        //     $reply_token = $inputs['events'][0]['replyToken'];
 
-            // LINEBOTSDKの設定
-            $http_client = new CurlHTTPClient(config('services.line.channel_token'));
-            $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
+        //     // LINEBOTSDKの設定
+        //     $http_client = new CurlHTTPClient(config('services.line.channel_token'));
+        //     $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
 
-            // 送信するメッセージの設定
-            $reply_message = $message_content . 'メッセージありがとうございます';
+        //     // 送信するメッセージの設定
+        //     $reply_message = $message_content . 'メッセージありがとうございます';
 
-            // if message content is `確認`
-            if ($message_content == '確認') {
-                $reply_message = 'ご確認ありがとうございます';
-            }
+        //     // if message content is `確認`
+        //     if ($message_content == '確認') {
+        //         $reply_message = 'ご確認ありがとうございます';
+        //     }
 
-            // ユーザーにメッセージを返す
-            $reply = $bot->replyText($reply_token, $reply_message);
-            Log::info("LOG: the msg is sent");
+        //     // ユーザーにメッセージを返す
+        //     $reply = $bot->replyText($reply_token, $reply_message);
 
+        //     // LINEのユーザーIDをuserIdに代入
+        //     $userId = $request['events'][0]['source']['userId'];
 
-            // LINEのユーザーIDをuserIdに代入
-            $userId = $request['events'][0]['source']['userId'];
+        //     // userIdがあるユーザーを検索
+        //     $user = User::where('line_id', $userId)->first();
 
-            // userIdがあるユーザーを検索
-            $user = User::where('line_id', $userId)->first();
+        //     // もし見つからない場合は、データベースに保存
+        //     if ($user == NULL) {
+        //         $profile = $bot->getProfile($userId)->getJSONDecodedBody();
 
-            // もし見つからない場合は、データベースに保存
-            if ($user == NULL) {
-                $profile = $bot->getProfile($userId)->getJSONDecodedBody();
-
-                $user = new User();
-                $user->provider = 'line';
-                $user->line_id = $userId;
-                $user->name = $profile['displayName'];
-                $user->save();
-
-                Log::info("LOG: the user is saved " . $userId);
-            }
-            return 'ok';
-        }
+        //         $user = new User();
+        //         $user->provider = 'line';
+        //         $user->line_id = $userId;
+        //         $user->name = $profile['displayName'];
+        //         $user->save();
+        //     }
+        //     return 'ok';
+        // }
     }
 
 
