aada348128417b75bd707570e15287cad69a71b3
Hashem Nazar Ono <hashem.nazar@gmail.com>
2022-02-12 18:11:18 +0900

    updated ROUTESERVICE PROVIDER for accounts controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AccountsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LineMessengerController;
 use App\Http\Controllers\LiffController;
@@ -19,6 +20,7 @@ use App\Http\Controllers\DashboardController;
 */
 
 
+
 Auth::routes();
 
 
@@ -29,10 +31,11 @@ Route::get('/settings', [DashboardController::class, 'settings'])->name('setting
 Route::get('/billing', [DashboardController::class, 'billing'])->name('billing');
 Route::get('/wizard', [DashboardController::class, 'wizard'])->name('wizard');
 Route::get('/friends', [DashboardController::class, 'friends'])->name('friends');
-Route::get('/accounts-list', [DashboardController::class, 'accountsList'])->name('accounts');
-Route::get('/account', [DashboardController::class, 'account'])->name('account');
 Route::get('/chat', [DashboardController::class, 'chat'])->name('chat');
 
+Route::resource('/accounts', AccountsController::class);
+Route::get('/accounts-list', [DashboardController::class, 'accountsList'])->name('accounts');
+Route::get('/account', [DashboardController::class, 'account'])->name('account');
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
