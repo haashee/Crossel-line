5f04f1504d2f571642f07fe0ca797fd0a797bcda
Hashem Nazar Ono <hashem.nazar@gmail.com>
2022-02-12 18:32:16 +0900

    created accounts table migration

---
+++ database/migrations/2022_02_12_092014_create_accounts.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccounts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('image')->nullable();
+            $table->mediumText('channel_secret')->nullable();
+            $table->mediumText('access_token')->nullable();
+            $table->string('endpoint')->nullable();
+            $table->string('webhook')->nullable();
+            $table->timestamps();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
