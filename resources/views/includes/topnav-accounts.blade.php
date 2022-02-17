a2ce41e6b9aa880855d4af18bda0e7b16e8ae321
Hashem Nazar Ono <hashem.nazar@gmail.com>
2022-02-17 15:25:03 +0900

    updated topnav for chat

--- resources/views/includes/topnav-accounts.blade.php
+++ resources/views/includes/topnav-accounts.blade.php
@@ -1,17 +1,15 @@
 <div class="col-auto my-sm-auto ms-sm-auto me-sm-0 mx-auto mt-3">
     <div class="nav-wrapper position-relative end-0">
-        <ul class="nav nav-pills nav-fill p-1" role="tablist">
-            <li class="nav-item mx-3">
+        <ul class="nav nav-pills-custom nav-fill p-1" role="tablist">
+            <li class="nav-item mx-2">
                 <a 
-                    class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center
-                        {{ Route::currentRouteNamed('accounts.index') ? 'topnav-select' : '' }}"
-                    href="{{ URL::route('accounts.index') }}" role="tab" aria-selected="false">
-                    {{-- <i class="ni ni-app"></i> --}}
+                    class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center {{ Route::currentRouteNamed('chat.index') ? 'topnav-select' : '' }}"
+                    href="{{ route('chat.index', ['aid' => $account->id]) }}" role="tab" aria-selected="true">
                     <i class="ni ni-chat-round"></i>
                     <span class="ms-2">チャット機能</span>
                 </a>
             </li>
-            <li class="nav-item mx-3">
+            <li class="nav-item mx-2">
                 <a 
                     class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center 
                         {{ Route::currentRouteNamed('friends.index') ? 'topnav-select' : '' }}"
@@ -29,7 +27,7 @@
                     <span class="ms-2">リッチメニュー</span>
                 </a>
             </li>
-            <li class="nav-item mx-3">
+            <li class="nav-item mx-2">
                 <a 
                     class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center
                         {{ Route::currentRouteNamed('accounts.edit') ? 'topnav-select' : '' }}"
