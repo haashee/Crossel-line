eebfd03e0a304f1cee9cef537c9f2e69a6d15fd4
Hashem Nazar Ono <hashem.nazar@gmail.com>
2022-02-22 14:59:07 +0900

    updated styling on membership

--- resources/views/dashboard/membership/index.blade.php
+++ resources/views/dashboard/membership/index.blade.php
@@ -878,20 +878,8 @@
   </nav> --}}
   <!-- End Navbar -->
   <main class="main-content  mt-0">
-    <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
-      style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg'); background-position: top;">
-      <span class="mask bg-gradient-dark opacity-6"></span>
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-5 text-center mx-auto">
-            <h1 class="text-white mb-2 mt-5">Welcome!</h1>
-            <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for
-              free.</p>
-          </div>
-        </div>
-      </div>
-    </div>
 
+    
     <!--session message-->
     @if (session()->has('message'))
     <div class="position-fixed bottom-1 end-1 z-index-2">
@@ -910,10 +898,30 @@
       </div>
     </div>
     @endif
+
+
+    
+    <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
+      style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg'); background-position: top;">
+      <span class="mask bg-gradient-dark opacity-6"></span>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-5 text-center mx-auto">
+            <h1 class="text-white mb-2">プロフィール</h1>
+            {{-- <p class="text-lead text-white">
+              Use these awesome forms to login or create new account in your project for free.
+            </p> --}}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+
     <div class="container">
-      <div class="row mt-lg-n10 mt-md-n11 mt-n10 justify-content-center pb-5">
+      <div class="row mt-lg-n10 mt-md-n11 mt-n14 justify-content-center pb-5">
         <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
-          <div class="card z-index-0">
+          <div class="card z-index-0 membership-card-height">
             {{-- <div class="card-header text-center pt-4">
               <h5>Register with</h5>
             </div> --}}
